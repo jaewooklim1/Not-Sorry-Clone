38a484a3b322bcc7cedf60b93014deb81792976f
Anug Saha <anugsaha@gmail.com>
2021-05-24 13:18:42 -0400

    create session reducers and actions

---
+++ frontend/src/components/main/splash.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Splash extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>NOT SORRY</h1>
+        <footer>
+          Created by JACM Productions
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Splash;
