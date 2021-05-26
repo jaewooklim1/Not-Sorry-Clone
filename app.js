3d8b7a8f981937a61e812d2720d1a8b910ad83ef
jaewooklim1 <jaewooklim1@gmail.com>
2021-05-26 11:12:47 -0400

    changed build to public

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ const users = require("./routes/api/users");
 const rooms = require("./routes/api/rooms");
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-  }
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -65,7 +59,14 @@ app.use("/api/users", users);
 app.use("/api/rooms", rooms);
 // app.use("/api/messages", messages);
 
-
-
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/public'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+  })
+}
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
