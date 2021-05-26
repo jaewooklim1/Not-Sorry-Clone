3ae3781265b91419bd628b0966d85ddbf10047f4
jaewooklim1 <jaewooklim1@gmail.com>
2021-05-26 11:06:21 -0400

    moved from backend app to frontend app

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -8,6 +8,13 @@ import RoomsContainer from './main/rooms_container'
 import Splash from './main/splash';
 import Game from './games/Game';
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
+  
 const App = () => (
     <div>
         <NavBarContainer />
