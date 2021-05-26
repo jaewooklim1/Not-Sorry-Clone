7ccd6b20e4b6175697fa9d7ac0b666aa73ffc07a
jaewooklim1 <jaewooklim1@gmail.com>
2021-05-26 10:22:37 -0400

    changed constant name from options to opts

--- config/passport.js
+++ config/passport.js
@@ -3,14 +3,14 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const keys = require('./keys');
-require('dotenv').config();
+// require('dotenv').config();
 
-const options = {};
-options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = keys.secretOrKey;
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-  passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
     User.findById(jwt_payload.id)
       .then(user => {
         if (user) {
