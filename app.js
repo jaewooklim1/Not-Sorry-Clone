cff63781cd814e8f6a42b8aa71319aa19db09976
jaewooklim1 <jaewooklim1@gmail.com>
2021-05-26 11:23:46 -0400

    changed app.js to build

--- app.js
+++ app.js
@@ -62,9 +62,9 @@ app.use("/api/rooms", rooms);
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/public'));
+  app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
 
