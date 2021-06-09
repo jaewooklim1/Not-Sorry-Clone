0f0730e7d9d53c212e4edf252564569265b813ad
jaewooklim1 <jaewooklim1@gmail.com>
2021-06-09 11:57:30 -0400

    changes

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://cmwinner89:p5lznpmldoF6GSLV@notsorry.gdt9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    //Make sure this is your own unique string
-    secretOrKey: "secret"
-}
